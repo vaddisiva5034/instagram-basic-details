9cf166bccad14a0ee6f599fd7901fb3fc99a5fd5
Kiran Vaddi <Kiran_Vaddi@epam.com>
2021-07-20 11:13:05 +0530

    updated the compo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Home } from "./Components/Home";
+import { Auth } from "./Components/Auth";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Switch>
+          <Route path="/home">
+            <Home></Home>
+          </Route>
+          <Route path="/auth">
+            <Auth />
+          </Route>
+          <Route path="/" exact>
+            <Home></Home>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/Components/Auth.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export function Auth() {
+  return (
+    <div>
+      <h1>Auth componet</h1>
+    </div>
+  );
+}
