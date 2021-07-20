599e000f34cfe4866ff592dac2523bacb5726a56
Kiran Vaddi <Kiran_Vaddi@epam.com>
2021-07-20 12:26:22 +0530

    some

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { Home } from "./Components/Home";
 import { Auth } from "./Components/Auth";
 function App() {
+  const url =
+    "https://api.instagram.com/oauth/authorize?client_id=626807078280160&redirect_uri=https://master.d2fobv91sb7b4f.amplifyapp.com/auth&scope=user_profile,user_media&response_type=code";
   return (
     <div className="App">
       <Router>
@@ -18,6 +20,7 @@ function App() {
           </Route>
         </Switch>
       </Router>
+      <a href={url}>Instagram</a>
     </div>
   );
 }
--- src/Components/Auth.jsx
+++ src/Components/Auth.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
+import queryString from "query-string";
 
 export function Auth() {
+  const location = useLocation();
+  const queryParms = queryString.parse(location.search);
+
   return (
     <div>
-      <h1>Auth componet</h1>
+      <h1>Auth componet - {queryParms?.code}</h1>
     </div>
   );
 }
